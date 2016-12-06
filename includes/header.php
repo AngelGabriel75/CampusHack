8445186c99a0e75d9d51860863e5b33cb9b07058
Angel Gabriel <areyes@uprb.edu>
2016-12-05 20:37:58 -0400

    UploadDec5_8:37pm

--- includes/header.php
+++ includes/header.php
@@ -49,9 +49,10 @@ if(isset($_SESSION['username'])){
 			<i class="fa fa-home fa-lg"></i>
 		</a>
 
-		<a href="#">
+		<a href="javascript:void(0);" onclick="getDropdownData('<?php echo $userLoggedIn; ?>', 'message')">
 		<i class="fa fa-envelope fa-lg"></i>
 		</a>
+
 		<a href="#">
 		<i class="fa fa-bell-o fa-lg"></i>
 		</a>
@@ -67,7 +68,9 @@ if(isset($_SESSION['username'])){
 
 
 	</nav>
-	
+
+	<div class="dropdown_data_window"></div>
+	<input type="hidden" id="dropdown_data_type" value="">
 
 </div>
 
